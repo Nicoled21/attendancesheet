be7804106c72c254698a28ae80338d6f6fd77265
Nicole D <52378875+Nicoled21@users.noreply.github.com>
2019-11-18 20:57:53 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentDetailsComponent } from './students/student-details/student-details.component';
+import { StudentListComponent } from './students/student-list/student-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentDetailsComponent,
+    StudentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
